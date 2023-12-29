4570373078fa7461df49c29843c7641169a18d12
Vinicius Campos <vinicius_cs_01@hotmail.com>
2023-12-28 23:17:52 -0300

    Pequenas modificacoes

--- controllers/PlanilhaController.js
+++ controllers/PlanilhaController.js
@@ -9,41 +9,53 @@ module.exports = class PlanilhaController {
     }
 
     static async importar(req, res) {
-        try {
-            if (!req.file) {
-                return res.status(400).send('Nenhum arquivo enviado.');
-            }
-            const planilhaPath = req.file.path;
 
-            await PlanilhaController.tratarPlanilha(planilhaPath);
-        } catch (err) {
-            console.log(err);
-            return res.status(500).send('Erro ao importar planilha.');
+        if (!req.file) {
+            return res.status(400).send('Nenhum arquivo enviado.');
         }
+        const planilhaPath = req.file.path;
+
+        await PlanilhaController.tratarPlanilha(planilhaPath)
+            .then(() => {
+                res.send('Planilha importada com sucesso.');
+                console.log();
+            })
+            .catch(err => {
+                res.send(`Erro ao importar planilha. ${err}`);
+                console.log(err);
+            });
+
     }
 
     static async tratarPlanilha(planilhaPath) {
-        const workbook = new excelJs.Workbook();
-        await workbook.csv.readFile(planilhaPath);
+        return new Promise(async (resolve, reject) => {
+            try {
+                const workbook = new excelJs.Workbook();
+                await workbook.csv.readFile(planilhaPath);
 
-        const header = ['Disciplina;RESP_1;RESP_2;RESP_3;RESP_4;RESP_5;MEDIA;COD QUESTAO;TITULO;MEDIA GERAL;NUMERO PREENCHIMENTO']
-        const planilhas = [];
-        workbook.eachSheet((sheet, sheetId) => {
-            sheet.eachRow((row, rowNumber) => {
-                const disciplina = row.getCell(1).value;
-                const valores = disciplina.split(';');
-                console.log(valores[0]);
-                if (!planilhas[valores[0]]) {
-                    planilhas[valores[0]] = new excelJs.Workbook();
-                    planilhas[valores[0]].addWorksheet(sheet.name);
-                    planilhas[valores[0]].getWorksheet(sheet.name).addRow(header);
+                const header = ['Disciplina;RESP_1;RESP_2;RESP_3;RESP_4;RESP_5;MEDIA;COD QUESTAO;TITULO;MEDIA GERAL;NUMERO PREENCHIMENTO']
+                const planilhas = [];
+                workbook.eachSheet((sheet, sheetId) => {
+                    sheet.eachRow((row, rowNumber) => {
+                        const disciplina = row.getCell(1).value;
+                        const valores = disciplina.split(';');
+                       // console.log(valores[0]);
+                        if (!planilhas[valores[0]]) {
+                            planilhas[valores[0]] = new excelJs.Workbook();
+                            planilhas[valores[0]].addWorksheet(sheet.name);
+                            planilhas[valores[0]].getWorksheet(sheet.name).addRow(header);
+                        }
+                        planilhas[valores[0]].getWorksheet(sheet.name).addRow(row.values);
+                    })
+                });
+                for (const disciplina in planilhas) {
+                    const outputPath = `archives/${disciplina[0]}.csv`;
+                    await planilhas[disciplina].csv.writeFile(outputPath);
                 }
-                planilhas[valores[0]].getWorksheet(sheet.name).addRow(row.values);
-            })
+                resolve();
+            } catch (err) {
+                reject(err); // Rejeite a Promise com o motivo do erro
+            }
         });
-        for (const disciplina in planilhas) {
-            const outputPath = `planilha_${disciplina[0]}.csv`;
-            await planilhas[disciplina].csv.writeFile(outputPath);
-        }
     }
 }
