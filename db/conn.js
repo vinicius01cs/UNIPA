d221327d6ca7c7f2a32e21c54684b8b1e1e5fc62
Vinicius Campos <vinicius_cs_01@hotmail.com>
2024-02-17 19:01:57 -0300

    Adiciona variaveis de ambiente

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('automatizacpa', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+    host: process.env.DATABASE_HOST,
     dialect: 'mysql'
 })
 
