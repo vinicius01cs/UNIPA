9819037fefb490ee44f220df245ca00b294ae3e0
Vinicius Campos <vinicius_cs_01@hotmail.com>
2024-09-07 15:24:42 -0300

    feat: Renderiza diferentes paginas iniciais de acordo com o nivel de autenticacao do usuario

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,5 +1,10 @@
 module.exports = class HomeController {
-    static index (req, res){
-        res.render('home/index');
+    static index(req, res) {
+        if (req.user.tipoUsuario === 1) {
+            res.render('home/index');
+        }
+        else{
+            res.render('home/indexAluno');
+        }
     }
 }
