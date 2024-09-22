751c5ef6682a91e585cbde899026a26e9f3389c4
Vinicius Campos <vinicius_cs_01@hotmail.com>
2024-09-22 12:45:43 -0300

    refactor: altera o modelo do gpt para o gpt-4-turbo

--- controllers/OpenAIController.js
+++ controllers/OpenAIController.js
@@ -13,7 +13,7 @@ module.exports = class OpenAIController {
     static async ConsultarGpt(prompt) {
         try {
             const completion = await openai.chat.completions.create({
-                model: "gpt-3.5-turbo-0125",
+                model: "gpt-4-turbo",
                 messages: [{ role: 'user', content: prompt }],
             });
 
